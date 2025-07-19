a609d8431f5474236dcdbb95b3d0a1a890acd82f
Brandon Rafael Lovelyno <110889867+BrandonRafaelLovelyno@users.noreply.github.com>
2025-07-19 17:38:12 +0700

    0917-BOATS-TO-SAVE-PEOPLE Stats: Time: 9 ms (75.38%), Space: 9 MB (73.08%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.go
+++ 0917-boats-to-save-people/0917-boats-to-save-people.go
@@ -3,14 +3,13 @@ import (
 )
 
 func numRescueBoats(people []int, limit int) int {
-    sorted_people := people
-    sort.Ints(sorted_people)
+    sort.Ints(people)
     
     p1, p2 := 0, len(people) - 1
     ans := 0
 
     for p1 <= p2 {
-        if sorted_people[p2] + sorted_people[p1] > limit{
+        if people[p2] + people[p1] > limit{
             p2--
         }else{
             p2--
