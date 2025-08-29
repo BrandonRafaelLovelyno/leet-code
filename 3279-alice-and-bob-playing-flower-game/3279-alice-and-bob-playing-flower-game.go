b38155a35d43ce611b9596c7b90a341d8ac944fd
Brandon Rafael Lovelyno <110889867+BrandonRafaelLovelyno@users.noreply.github.com>
2025-08-29 22:16:58 +0700

    3279-ALICE-AND-BOB-PLAYING-FLOWER-GAME Stats: Time: 0 ms (100%), Space: 4.1 MB (16.67%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.go
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.go
@@ -1,19 +1,8 @@
 func flowerGame(n int, m int) int64 {
-    nOdd, nEven, mOdd, mEven := 0,0,0,0
+	nOdd := (n + 1) / 2
+	nEven := n / 2
+	mOdd := (m + 1) / 2
+	mEven := m / 2
 
-    if n % 2 == 1{
-        nOdd = n/2 + 1
-    }else{
-        nOdd = n/2
-    }
-    nEven = n - nOdd
-
-    if m % 2 == 1 {
-        mOdd = m/2 + 1
-    }else{
-        mOdd = m/2
-    }
-    mEven = m - mOdd
-
-    return int64(nOdd * mEven + mOdd * nEven)
+	return int64(nOdd*mEven + mOdd*nEven)
 }
