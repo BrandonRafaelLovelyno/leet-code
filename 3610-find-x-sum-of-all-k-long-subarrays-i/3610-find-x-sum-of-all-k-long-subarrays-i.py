daf62068fb021dc7a93a2773f841c61f6f082bbf
Brandon Rafael Lovelyno <110889867+BrandonRafaelLovelyno@users.noreply.github.com>
2025-11-05 12:02:39 +0700

    3610-FIND-X-SUM-OF-ALL-K-LONG-SUBARRAYS-I Stats: Time: 23 ms (74.29%), Space: 17.9 MB (38.82%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,23 +1,18 @@
+from collections import Counter
+
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        freq = {}
-        answer = []
-        left, right = 0, k-1
+        n = len(nums)
+        freq = Counter(nums[:k])
 
-        while right < len(nums):
-            if left == 0:
-                for i in range(k):
-                    freq[nums[i]] = 1 if nums[i] not in freq else freq[nums[i]] + 1
-            else:
-                freq[nums[left - 1]] -= 1
-                freq[nums[right]] = 1 if nums[right] not in freq else freq[nums[right]] + 1
-            count, ans = 0, 0
-            for k, v in sorted(freq.items(), key= lambda i: (i[1],i[0]), reverse=True):
-                if count == x: break
-                ans += v * k
-                count += 1
-            answer.append(ans)
-            right+=1
-            left+=1
+        ans = []
+        for i in range(n-k+1):
+            top = sorted(freq.items(), key = lambda i: (i[1],i[0]), reverse=True)[:x]
+            ans.append(sum(f * n for f, n in top))
+            
+            if i + k < n:
+                left, new = nums[i], nums[i+k]
+                freq[left] -= 1
+                freq[new] = 1 if new not in freq else freq[new] + 1
 
-        return answer
+        return ans
