37ed7f93b833601bc6f912dce9cfa50884430cfa
Brandon Rafael Lovelyno <110889867+BrandonRafaelLovelyno@users.noreply.github.com>
2025-11-23 21:20:58 +0700

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 3 ms (90.34%), Space: 21.3 MB (61.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,45 @@
 from collections import Counter
 
+
+def create_max_heap(heap):
+    n = len(heap)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(heap, i)
+
+
+def heapify(heap, i):
+    biggest = i
+    n = len(heap)
+
+    if 2 * i + 1 < n and heap[biggest][1] < heap[2 * i + 1][1]:
+        biggest = 2 * i + 1
+    if 2 * i + 2 < n and heap[biggest][1] < heap[2 * i + 2][1]:
+        biggest = 2 * i + 2
+
+    if biggest != i:
+        heap[i], heap[biggest] = heap[biggest], heap[i]
+        heapify(heap, biggest)
+
+
+def pop_heap(heap):
+    root = heap[0]
+    last = heap.pop()
+
+    if heap:
+        heap[0] = last
+        heapify(heap, 0)
+    
+    return root
+
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq_dict = Counter(nums)
+        heap = list(freq_dict.items())
+        create_max_heap(heap)
 
         ans = []
-        i = 0
-        for num, freq in sorted(freq_dict.items(), key = lambda x: x[1], reverse = True):
-            ans.append(num)
-            i+=1
-            if i == k:
-                break
+        for i in range(k):
+            ans.append(pop_heap(heap)[0])
+
         return ans
