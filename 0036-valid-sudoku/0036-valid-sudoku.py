d2609b06dfa2a6c598795dd2bf4b338c904e678a
Brandon Rafael Lovelyno <110889867+BrandonRafaelLovelyno@users.noreply.github.com>
2025-12-14 16:22:57 +0700

    0036-VALID-SUDOKU Stats: Time: 4 ms (56.46%), Space: 18 MB (5.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,22 +1,10 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(0,3):
-            for j in range(0,3):
-                x1, y1 = i * 3, j * 3
-                my_set = set()
-                for k in range(x1, x1 + 3):
-                    for l in range(y1, y1 + 3):
-                        if board[k][l] != "." and board[k][l] in my_set:
-                            return False
-                        my_set.add(board[k][l])
-
+        tuples = []
         for i in range(0, 9):
-            for j in range(0,9):
-                if board[i][j] != ".":
-                    for k in range(i+1, 9):
-                        if board[k][j] == board[i][j]:
-                            return False
-                    for k in range(j+1, 9):
-                        if board[i][k] == board[i][j]:
-                            return False
-        return True
+            for j in range(0, 9):
+                element = board[i][j]
+                if element == ".":
+                    continue
+                tuples += [(i, element), (element, j), (i//3, j//3, element)]
+        return len(tuples)== len(set(tuples))
