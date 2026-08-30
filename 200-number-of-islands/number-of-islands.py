f470f10af08924dd71f5f3db7de4c2e6be281c4c
Brandon Rafael Lovelyno <110889867+BrandonRafaelLovelyno@users.noreply.github.com>
2026-08-30 21:21:14 +0700

    Time: 238 ms (72.77%) | Memory: 21.7 MB (36.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,13 +2,11 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         row, col = len(grid), len(grid[0])
 
-        is_marked = [[False for _ in range(col)] for _ in range(row)]
-
         def dfs(grid, r, c):
-            if r >= row or r < 0 or c >= col or c < 0 or grid[r][c] == "0" or is_marked[r][c]:
+            if r >= row or r < 0 or c >= col or c < 0 or grid[r][c] == "0":
                 return
 
-            is_marked[r][c] = True
+            grid[r][c] = "0"
 
             dfs(grid, r + 1, c)
             dfs(grid, r - 1, c)
@@ -18,7 +16,7 @@ class Solution:
         ans = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == "1" and not is_marked[r][c]:
+                if grid[r][c] == "1":
                     ans += 1
                     dfs(grid, r, c)
 
