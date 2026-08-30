14706da76473ede55c7b8a5127291ccee8cbb34e
Brandon Rafael Lovelyno <110889867+BrandonRafaelLovelyno@users.noreply.github.com>
2026-08-30 16:02:41 +0700

    Time: 13 ms (10.68%) | Memory: 23.3 MB (12.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,14 +2,11 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         clean_s = ''.join(c.lower() for c in s if c.isalnum())
         
-        stack = [clean_s[i] for i in range(len(clean_s)//2)]
+        p_start, p_end = 0, len(clean_s) - 1
 
-        start_i = len(clean_s) // 2
-        if len(clean_s) % 2 != 0:
-            start_i += 1
-
-        for i in range(start_i, len(clean_s)):
-            if clean_s[i] != stack.pop():
+        while p_start < p_end:
+            if clean_s[p_start] != clean_s[p_end]:
                 return False
+            p_start, p_end = p_start + 1, p_end - 1
         
         return True
